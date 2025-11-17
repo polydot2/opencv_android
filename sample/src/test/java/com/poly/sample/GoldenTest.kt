e996c5d619b118b0c054f181d8b9498ddf832bcb
Jérémy Girerd-Rey <jeremy.girerdrey@gmail.com>
2025-11-18 00:17:09 +0100

    Fix cpp

--- sample/src/test/java/com/poly/sample/GoldenTest.kt
+++ sample/src/test/java/com/poly/sample/GoldenTest.kt
@@ -1,60 +1,71 @@
 package com.poly.sample
 
-import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import com.github.takahirom.roborazzi.RoborazziOptions
 import com.github.takahirom.roborazzi.captureRoboImage
 import com.poly.opencv.OpencvLib
+import org.junit.Assert
+import org.junit.BeforeClass
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.opencv.android.OpenCVLoader
 import org.opencv.android.Utils.bitmapToMat
 import org.opencv.android.Utils.matToBitmap
 import org.opencv.core.CvType
 import org.opencv.core.Mat
 import org.opencv.core.Size
 import org.robolectric.RobolectricTestRunner
-import org.robolectric.RuntimeEnvironment
-import org.robolectric.RuntimeEnvironment.application
-import org.robolectric.RuntimeEnvironment.getApplication
 import org.robolectric.annotation.GraphicsMode
-import java.io.InputStream
 
 @RunWith(RobolectricTestRunner::class)
 @GraphicsMode(GraphicsMode.Mode.NATIVE)  // Rendering natif pour diffs exacts
 class GoldenTest {
 
+    companion object {
+        @JvmStatic
+        @BeforeClass
+        fun loadLibraries() {
+            val opencvLoaded = OpenCVLoader.initLocal()
+            Assert.assertTrue(
+                "ÉCHEC CRITIQUE: La bibliothèque native d'OpenCV (libopencv_java4.so) n'a pas pu être chargée.",
+                opencvLoaded
+            )
+//            System.loadLibrary("OpencvLib")
+        }
+    }
+
     @Test
     fun testAdaptiveThreshold() {
-        val context: Context = getApplication()
+        val inputStream = GoldenTest::class.java.classLoader?.getResourceAsStream("input/lenna_input.png")
 
-        context.assets.open("input/lenna_input.png").use { inputStream: InputStream ->
-            val inputBitmap = BitmapFactory.decodeStream(inputStream)
+        val inputBitmap = BitmapFactory.decodeStream(inputStream)
 
-            // Créer Mats (CV_8UC1 pour gray, ou CV_8UC3 pour color)
-            val matInput = Mat(Size(inputBitmap.width.toDouble(), inputBitmap.height.toDouble()), CvType.CV_8UC1)
-            val matOutput = Mat(Size(inputBitmap.width.toDouble(), inputBitmap.height.toDouble()), CvType.CV_8UC1)
+        // Créer Mats (CV_8UC1 pour gray, ou CV_8UC3 pour color)
+        val matInput = Mat(Size(inputBitmap.width.toDouble(), inputBitmap.height.toDouble()), CvType.CV_8UC1)
+        val matOutput = Mat(Size(inputBitmap.width.toDouble(), inputBitmap.height.toDouble()), CvType.CV_8UC1)
 
-            // Bitmap to Mat
-            bitmapToMat(inputBitmap, matInput)
+        // Bitmap to Mat
+        bitmapToMat(inputBitmap, matInput)
 
-            // Run JNI
-            OpencvLib.adaptiveThreshold(matInput.nativeObj, matOutput.nativeObj)
+        // Run JNI
+        OpencvLib.adaptiveThreshold(matInput.nativeObj, matOutput.nativeObj)
 
-            // Mat to Bitmap
-            val outputBitmap = Bitmap.createBitmap(matOutput.cols(), matOutput.rows(), Bitmap.Config.ARGB_8888)
-            matToBitmap(matOutput, outputBitmap)
+        // Mat to Bitmap
+        val outputBitmap = Bitmap.createBitmap(matOutput.cols(), matOutput.rows(), Bitmap.Config.ARGB_8888)
+        matToBitmap(matOutput, outputBitmap)
 
-            // Release Mats
-            matInput.release()
-            matOutput.release()
+        // Release Mats
+        matInput.release()
+        matOutput.release()
 
-            outputBitmap.captureRoboImage(
-                filePath = "Lenna_output",
-                roborazziOptions = RoborazziOptions(
-                    compareOptions = RoborazziOptions.CompareOptions(changeThreshold = 0.02f)
-                )
+        outputBitmap.captureRoboImage(
+            filePath = "Lenna_output",
+            roborazziOptions = RoborazziOptions(
+                compareOptions = RoborazziOptions.CompareOptions(changeThreshold = 0.02f)
             )
-        }
+        )
+
+        inputStream?.close()
     }
 }
